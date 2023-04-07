5d9cad5d3169eac1437dd64ce0081d1985b5198b
Rodion Kotov <r.kotov@g.nsu.ru>
2023-04-07 10:34:43 +0700

    Добавил: - дизеринг Флойда - параметр выбора алгоритма дизеринга

--- src/jvmMain/kotlin/dialog/impl/tools/DitheringFilterDialog.kt
+++ src/jvmMain/kotlin/dialog/impl/tools/DitheringFilterDialog.kt
@@ -3,6 +3,7 @@ package ru.nsu.ccfit.plum.dialog.impl.tools
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import ru.nsu.ccfit.plum.component.rangeBox
+import ru.nsu.ccfit.plum.component.selectItem
 import ru.nsu.ccfit.plum.dialog.ToolDialog
 import ru.nsu.ccfit.plum.tool.filter.DitheringFilter
 import ru.nsu.ccfit.plum.tool.filter.SmoothingFilter
@@ -13,19 +14,23 @@ class DitheringFilterDialog : ToolDialog(DitheringFilter) {
     private val quantizationGreen = mutableStateOf(DitheringFilter.quantizationGreen)
     private val quantizationBlue = mutableStateOf(DitheringFilter.quantizationBlue)
 
-
+    private val dithering = mutableStateOf(DitheringFilter.dithering)
 
 
     override fun updateFilter() {
         DitheringFilter.quantizationRed = quantizationRed.value
         DitheringFilter.quantizationBlue = quantizationBlue.value
         DitheringFilter.quantizationGreen = quantizationGreen.value
+
+        DitheringFilter.dithering = dithering.value
     }
 
     @Composable
     override fun settingBox() {
-       rangeBox("Красный", quantizationRed,2..128)
-       rangeBox("Зеленый", quantizationGreen,2..128)
-       rangeBox("Синий", quantizationBlue,2..128)
+        selectItem("Алгоритм", dithering, DitheringFilter.Dithering.values())
+
+        rangeBox("Красный", quantizationRed, 2..128)
+        rangeBox("Зеленый", quantizationGreen, 2..128)
+        rangeBox("Синий", quantizationBlue, 2..128)
     }
 }
--- src/jvmMain/kotlin/tool/filter/DitheringFilter.kt
+++ src/jvmMain/kotlin/tool/filter/DitheringFilter.kt
@@ -1,78 +1,105 @@
 package ru.nsu.ccfit.plum.tool.filter
 
+import ru.nsu.ccfit.plum.component.Param
 import ru.nsu.ccfit.plum.component.PlumImage
-import ru.nsu.ccfit.plum.draw.getImageFilter
-import ru.nsu.ccfit.plum.draw.getIntRGB
 import java.awt.Color
-import java.lang.Error
-import kotlin.math.roundToInt
+import java.awt.image.BufferedImage
+
 
 object DitheringFilter : Filter("Дизеринг") {
 
+    enum class Dithering : Param {
+        FLOYD_STEINBERG,
+        ORDERED;
+
+        override fun getName(): String {
+            return toString()
+        }
+    }
+
+    var dithering = Dithering.FLOYD_STEINBERG
     var quantizationRed = 2
     var quantizationBlue = 2
     var quantizationGreen = 2
 
 
-    fun ditheringFloydSteinberg(image: PlumImage, numColors: Int) {
-        val quantError = Array(image.height) { FloatArray(image.width) } // quantization error buffer
-        val colorStep = 256 / numColors // color step size
-
-        for (y in 0 until image.height) {
-            for (x in 0 until image.width) {
-                val oldColor = Color(image.getRGB(x, y))
-                val newColor = Color(
-                    (oldColor.red / colorStep) * colorStep, // round red component
-                    (oldColor.green / colorStep) * colorStep, // round green component
-                    (oldColor.blue / colorStep) * colorStep // round blue component
-                )
-                image.setRGB(x, y, newColor.rgb)
-
-                // compute quantization error
-                val quantErrorR = oldColor.red - newColor.red
-                val quantErrorG = oldColor.green - newColor.green
-                val quantErrorB = oldColor.blue - newColor.blue
-
-                // propagate quantization error to neighboring pixels
-                if (x < image.width - 1) {
-                    quantError[y][x + 1] += quantErrorR * 7.0f / 16.0f
-                    quantError[y][x + 1] += quantErrorG * 7.0f / 16.0f
-                    quantError[y][x + 1] += quantErrorB * 7.0f / 16.0f
-                }
-                if (y < image.height - 1) {
-                    if (x > 0) {
-                        quantError[y + 1][x - 1] += quantErrorR * 3.0f / 16.0f
-                        quantError[y + 1][x - 1] += quantErrorG * 3.0f / 16.0f
-                        quantError[y + 1][x - 1] += quantErrorB * 3.0f / 16.0f
-                    }
-                    quantError[y + 1][x] += quantErrorR * 5.0f / 16.0f
-                    quantError[y + 1][x] += quantErrorG * 5.0f / 16.0f
-                    quantError[y + 1][x] += quantErrorB * 5.0f / 16.0f
-                    if (x < image.width - 1) {
-                        quantError[y + 1][x + 1] += quantErrorR * 1.0f / 16.0f
-                        quantError[y + 1][x + 1] += quantErrorG * 1.0f / 16.0f
-                        quantError[y + 1][x + 1] += quantErrorB * 1.0f / 16.0f
-                    }
-                }
+    private fun ditheringFloydSteinberg(imageToProcess: PlumImage) {
+        val width = imageToProcess.width
+        val height = imageToProcess.height
+        var oldPixel: Color
+        for (i in 0 until height) {
+            for (j in 0 until width) {
+                oldPixel = Color(imageToProcess.getRGB(j, i))
+                processPixel(imageToProcess, oldPixel, j, i)
+                spreadErrors(imageToProcess, oldPixel, j, i)
             }
         }
     }
-    fun setError(image: PlumImage, x: Int, y: Int, redError: Int, greenError: Int, blueError: Int, weight: Int) {
-        val (red, green, blue) = image.getPixel(x, y)
-        image.setRGB(
-            x,
-            y,
-            Triple(
-                red + redError * weight / 16,
-                green + greenError * weight / 16,
-                blue + blueError * weight / 16
-            ).getIntRGB()
+
+    private fun spreadErrors(image: PlumImage, oldPixel: Color, i: Int, j: Int) {
+        val newPixel = Color(image.getRGB(i, j))
+        val redError = oldPixel.red - newPixel.red
+        val greenError = oldPixel.green - newPixel.green
+        val blueError = oldPixel.blue - newPixel.blue
+        spreadErrorToPixel(image, i + 1, j, redError, greenError, blueError, 7 / 16.0)
+        spreadErrorToPixel(image, i - 1, j + 1, redError, greenError, blueError, 3 / 16.0)
+        spreadErrorToPixel(image, i, j + 1, redError, greenError, blueError, 5 / 16.0)
+        spreadErrorToPixel(image, i + 1, j + 1, redError, greenError, blueError, 1 / 16.0)
+    }
+
+    private fun processPixel(image: PlumImage, oldPixel: Color, i: Int, j: Int) {
+        val colorStepR = 255 / quantizationRed
+        val colorStepG = 255 / quantizationGreen
+        val colorStepB = 255 / quantizationBlue
+        val newColor: Color = Color(
+            (oldPixel.red / colorStepR) * colorStepR, // round red component
+            (oldPixel.green / colorStepG) * colorStepG, // round green component
+            (oldPixel.blue / colorStepB) * colorStepB // round blue component
         )
+        image.setRGB(i, j, newColor.rgb)
+    }
+
+    private fun spreadErrorToPixel(
+        image: PlumImage,
+        i: Int,
+        j: Int,
+        redError: Int,
+        greenError: Int,
+        blueError: Int,
+        multiplyValue: Double
+    ) {
+        if (outOfBounds(image, i, j)) {
+            //TODO:Обработать границы
+            return
+        }
+        val pixelToProcess = Color(image.getRGB(i, j))
+        val newRed = (pixelToProcess.red + redError * multiplyValue).toInt()
+        val newGreen = (pixelToProcess.green + greenError * multiplyValue).toInt()
+        val newBlue = (pixelToProcess.blue + blueError * multiplyValue).toInt()
+        val processedPixel = Color(normalizeColor(newRed), normalizeColor(newGreen), normalizeColor(newBlue))
+        image.setRGB(i, j, processedPixel.rgb)
+    }
+
+    private fun outOfBounds(image: BufferedImage, x: Int, y: Int): Boolean {
+        return x < 0 || y < 0 || x > image.width - 1 || y > image.height - 1
+    }
+
+    private fun normalizeColor(value: Int): Int {
+        if (value < 0) {
+            return 0
+        }
+        return if (value >= 255) {
+            255
+        } else value
     }
 
     override fun permit(image: PlumImage): PlumImage {
-        val newImage = GrayScaleFilter.permit(image)
-        ditheringFloydSteinberg(newImage, quantizationRed)
+        val newImage = image.copy()
+        when(dithering){
+            Dithering.FLOYD_STEINBERG ->  ditheringFloydSteinberg(newImage)
+            Dithering.ORDERED -> TODO()
+        }
+
         return newImage
     }
 }
