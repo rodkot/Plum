4b94aab6beef1bd148dad85e02efb1e767cae809
shift-lab-kuznetsov <e.kuznetsov1@g.nsu.ru>
2023-04-07 14:20:53 +0700

    Привел функцию к нормальному виду

--- src/jvmMain/kotlin/tool/filter/MirrorFilter.kt
+++ src/jvmMain/kotlin/tool/filter/MirrorFilter.kt
@@ -11,34 +11,33 @@ object MirrorFilter : Filter("Отражение") {
         val newImage = image.copy()
 
         when (alignment) {
-            Alignment.VERTICAL -> verticalReflection(newImage, image)
-            Alignment.HORIZONTAL -> horizontalReflection(newImage, image)
+            Alignment.VERTICAL -> reflection(newImage, image, Alignment.VERTICAL)
+            Alignment.HORIZONTAL -> reflection(newImage, image, Alignment.HORIZONTAL)
         }
 
         return newImage
     }
 
-    private fun horizontalReflection(newImage: PlumImage, image: PlumImage) {
+    private fun reflection(newImage: PlumImage, image: PlumImage, alignment: Alignment) {
         val height = image.height
         val width = image.width
 
         for (y in 0 until height) {
             for (x in 0 until width) {
-                newImage.setRGB(x, y, image.getRGB(x, height - y - 1))
+                newImage.setRGB(x, y, alignmentReflection(image, x, y, alignment))
             }
         }
     }
 
-    private fun verticalReflection(newImage: PlumImage, image: PlumImage) {
-        val height = image.height
-        val width = image.width
-
-        for (y in 0 until height) {
-            for (x in 0 until width) {
-                newImage.setRGB(x, y, image.getRGB(width - x - 1, y))
-            }
+    private fun alignmentReflection(
+        image: PlumImage,
+        x: Int,
+        y: Int,
+        alignment: Alignment
+    ) = when(alignment) {
+            Alignment.HORIZONTAL -> image.getRGB(x, image.height - y - 1)
+            Alignment.VERTICAL -> image.getRGB(image.width - x - 1, y)
         }
-    }
 
     enum class Alignment: Param {
         VERTICAL,
