d356dbc614b20d4547ef0b3d9357ba95a6799ed4
shift-lab-kuznetsov <e.kuznetsov1@g.nsu.ru>
2023-04-16 23:07:36 +0700

    Фильтр акварелизации, начало

---
+++ src/jvmMain/kotlin/tool/filter/AquarellFilter.kt
@@ -0,0 +1,9 @@
+package ru.nsu.ccfit.plum.tool.filter
+
+import ru.nsu.ccfit.plum.component.PlumImage
+
+object AquarellFilter: Filter("Фильтр акварелизации") {
+    override fun permit(image: PlumImage): PlumImage {
+        TODO("Not yet implemented")
+    }
+}
